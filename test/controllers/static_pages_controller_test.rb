455d98643bb4a1c746634cb37f9e1788325b5646
tim-t_miyata <t.miyata@logiclogic.jp>
2020-04-29 16:29:20 +0900

    rubocop -a

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,7 +7,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = 'Ruby on Rails Tutorial Sample App'
   end
 
-  test "should get root" do
+  test 'should get root' do
     get root_url
     assert_response :success
     assert_select 'title', "Home | #{@base_title}"

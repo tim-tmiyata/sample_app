d1711251ec803a59c56479ebfaed1023bd5dc900
tim-t_miyata <t.miyata@logiclogic.jp>
2020-05-04 18:03:52 +0900

    has_secure_passwordメソッドを追加 セキュアなパスワードの実装をする為

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@
 
 class User < ApplicationRecord
   before_save { self.email = email.downcase }
+  has_secure_password
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
   validates :email, presence: true, length: { maximum: 255 },

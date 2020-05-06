f5db00d2b0f0e5254e74b920cd46a239aad79e92
tim-t_miyata <t.miyata@logiclogic.jp>
2020-05-06 15:42:26 +0900

    ログイン失敗時の処理の実装

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   get '/signup',   to: 'users#new'
   post '/signup',  to: 'users#create'
   get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
   resources :users, only: %w[show create]
 end

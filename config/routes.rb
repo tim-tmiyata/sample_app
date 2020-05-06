11fec6a23a0c5b7db3ae721a948040b326846489
tim-t_miyata <t.miyata@logiclogic.jp>
2020-05-06 16:07:46 +0900

    ログインしているかどうかでヘッダーの出しわけをする

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
   post '/signup',  to: 'users#create'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users, only: %w[show create]
 end

74acea3730cdd3b16e41a6e6a699ee9628ba83d6
tim-t_miyata <t.miyata@logiclogic.jp>
2020-05-09 17:05:18 +0900

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_200_504_085_907) do
+ActiveRecord::Schema.define(version: 20_200_509_080_118) do
   create_table 'users', force: :cascade do |t|
     t.string 'name'
     t.string 'email'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
     t.string 'password_digest'
+    t.string 'remember_digest'
     t.index ['email'], name: 'index_users_on_email', unique: true
   end
 end

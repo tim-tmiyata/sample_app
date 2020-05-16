370d8fd474e3a15f2fd12741274970ee3424d5f7
tim-t_miyata <t.miyata@logiclogic.jp>
2020-05-16 17:38:32 +0900

    ユーザーを編集する機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,5 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  resources :users, only: %w[show create]
+  resources :users, only: %w[show create edit]
 end

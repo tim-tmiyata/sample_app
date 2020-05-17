f7009f7511c92119bef8c6976408bcbd48d0a4b6
tim-t_miyata <t.miyata@logiclogic.jp>
2020-05-17 16:35:59 +0900

    ユーザー情報のアップデート、更新ができるように

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: %i[edit update]
+
   def show
     @user = User.find(params[:id])
   end
@@ -41,5 +43,13 @@ class UsersController < ApplicationController
                                  :email,
                                  :password,
                                  :password_confirmation)
+  end
+
+  # ログイン済みユーザーかどうか確認
+  def logged_in_user
+    return if logged_in?
+
+    flash[:danger] = 'Please log in.'
+    redirect_to login_url
   end
 end

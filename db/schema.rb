728bc6e2d19a305b089b192129e5cf350bb7c96c
tim-t_miyata <t.miyata@logiclogic.jp>
2020-05-17 19:01:05 +0900

    adminカラムをユーザーテーブルに追加

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_200_509_080_118) do
+ActiveRecord::Schema.define(version: 20_200_517_095_845) do
   create_table 'users', force: :cascade do |t|
     t.string 'name'
     t.string 'email'
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20_200_509_080_118) do
     t.datetime 'updated_at', null: false
     t.string 'password_digest'
     t.string 'remember_digest'
+    t.boolean 'admin'
     t.index ['email'], name: 'index_users_on_email', unique: true
   end
 end

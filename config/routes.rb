4f19407d474ce783b69d5f147d4fb2d903210add
tim-t_miyata <t.miyata@logiclogic.jp>
2020-05-24 16:03:20 +0900

    アカウント有効化メールのテンプレート作成

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   resources :users, only: %w[index show create edit update destroy]
+  resources :account_activations, only: :edit
 end
